cd45d5815f7c71234957121cd22ae15d6bd15ec9
stillthinkingaboutit <46580797+stillthinkingaboutit@users.noreply.github.com>
2019-01-11 14:34:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-setInterval(signuptest,10);
 function signuptest(){
   var form = document.getElementById("signup");
   form.action = encodeURI("https://test--pixlperfect01.repl.co?username="+document.getElementById("usernameSignUp").value+"&password="+document.getElementById("passwordSignUp").value+"&email="+document.getElementById("email").value);
 }
 function verify(){
+  var t=document.getElementById("errs");
+  for(var i=0;i<t.children.length;i++){
+    t.children[i].style.hidden=true;
+  }
   var a = document.getElementById("usernameSignUp").value;
   var b = document.getElementById("passwordSignUp").value;
   var c = document.getElementById("confirmPassword").value;
   var d = document.getElementById("email").value;
   var e = document.getElementById("signup");
   if(a.length>=8){
-    if(b.length>=8&&c.length>=8){
+    if(b.length>=10&&c.length>=10){
       if(b===c){
         if(d.checkValidity()&&d.length!==0){
+          e.action = encodeURI("https://test--pixlperfect01.repl.co?username="+document.getElementById("usernameSignUp").value+"&password="+document.getElementById("passwordSignUp").value+"&email="+document.getElementById("email").value);
           e.submit();
         }else{
-          //invalid email
+          t.children[0].style.hidden=false;
         }
       }else{
-        //passwords do not match
+        t.children[1].style.hidden=false;
       }
     }else{
-      //password is not long enough
+      t.children[2].style.hidden=false;
     }
   }else{
-    //username is not long enough
+    t.children[3].style.hidden=false;
   }
 }
